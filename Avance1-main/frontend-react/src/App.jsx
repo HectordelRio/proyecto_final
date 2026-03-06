00f10fad27bb51fb2d24d2178b068d586c950c3f
HectordelRio <AL07020940@tecmilenio.mx>
2026-03-05 20:00:46 -0600

    A1

---
+++ Avance1-main/frontend-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola, mundo!</h1>
+      <p>La aplicación está funcionando correctamente.</p>
+    </div>
+  );
+}
+
+export default App;
